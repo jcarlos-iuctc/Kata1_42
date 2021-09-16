d3c680d28f88c8ffcc81eb607340e4732c2be1f7
Entrar <Entrar@PC-1133>
2021-09-16 09:56:54 +0100

    Primera versión de Kata1

---
+++ src/kata01_42/Main.java
@@ -0,0 +1,13 @@
+package kata01_42;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person person = new Person("Antonio", new Date(80, 0, 1));
+        
+        System.out.println(person.getName()+" tiene " + person.getAge() + " años.");
+    }
+    
+}
